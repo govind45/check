14bb679cf7f2f64c796bcbde405e20e41be46fd6
govind45 <govindarajulu920@gmail.com>
2016-01-06 13:07:51 +0530

    Update Home.java

--- src/Home.java
+++ src/Home.java
@@ -2,6 +2,6 @@ public class Home{
   public static void main(String[] args){
     
     System.out.println("This is first one.");
-    System.out.println("This is second check!")
+    System.out.println("This is second check!");
   }
 }
